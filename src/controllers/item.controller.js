0b78b5c459e05b1caaf291f48bf8395f2c4b411f
Norphel <113838216+norphel@users.noreply.github.com>
2024-03-22 21:50:35 +0530

    implement item delete and update controller and view

--- src/controllers/item.controller.js
+++ src/controllers/item.controller.js
@@ -82,4 +82,92 @@ const item_create_post = [
   }),
 ];
 
-export { item_list, item_detail, item_create_get, item_create_post };
+const item_delete_get = asyncHandler(async (req, res, next) => {
+  const item = await Item.findById(req.params.id).populate("category").exec();
+
+  if (item === null) {
+    res.redirect("/items");
+  }
+
+  res.render("item_delete", {
+    title: "Delete Item",
+    item: item,
+  });
+});
+
+const item_delete_post = asyncHandler(async (req, res, next) => {
+  await Item.findByIdAndDelete(req.params.id);
+  res.redirect("/items");
+});
+
+const item_update_get = asyncHandler(async (req, res, next) => {
+  const [item, allCategories] = await Promise.all([
+    Item.findById(req.params.id).populate("category").exec(),
+    Category.find({}, "name").sort({ name: 1 }).exec(),
+  ]);
+
+  if (item === null) {
+    const err = new Error("Item not found");
+    err.status = 404;
+    next(err);
+  }
+
+  res.render("item_form", {
+    title: "Update Item",
+    item: item,
+    allCategories: allCategories,
+    selected_category: item.category._id,
+  });
+});
+
+const item_update_post = [
+  body("name", "Item name is required").trim().isLength({ min: 1 }).escape(),
+  body("description", "Item description is required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("category").escape(),
+  body("price").isNumeric().escape(),
+  body("numberInStock", "Quantity in Stock should be an integer")
+    .isInt()
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      numberInStock: req.body.numberInStock,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      const allCategories = await Category.find({}, "name").sort({ name: 1 });
+
+      res.render("item_form", {
+        title: "Create Item",
+        allCategories: allCategories,
+        selected_category: item.category._id,
+        item: item,
+      });
+      return;
+    } else {
+      const updatedItem = await Item.findByIdAndUpdate(req.params.id, item, {});
+      res.redirect("/items" + updatedItem.url);
+    }
+  }),
+];
+
+export {
+  item_list,
+  item_detail,
+  item_create_get,
+  item_create_post,
+  item_delete_get,
+  item_delete_post,
+  item_update_get,
+  item_update_post,
+};
--- src/routes/item.routes.js
+++ src/routes/item.routes.js
@@ -6,11 +6,19 @@ import {
   item_detail,
   item_create_get,
   item_create_post,
+  item_delete_get,
+  item_delete_post,
+  item_update_get,
+  item_update_post,
 } from "../controllers/item.controller.js";
 
 router.get("/", item_list);
 router.get("/item/create", item_create_get);
 router.post("/item/create", item_create_post);
 router.get("/item/:id", item_detail);
+router.get("/item/:id/delete", item_delete_get);
+router.post("/item/:id/delete", item_delete_post);
+router.get("/item/:id/update", item_update_get);
+router.post("/item/:id/update", item_update_post);
 
 export default router;
